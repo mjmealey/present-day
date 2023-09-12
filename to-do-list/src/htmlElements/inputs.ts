e51c23a21f98fa2d9ea1b34d9cd5354b35d86763
mjmealey <markjmealey@gmail.com>
2023-09-11 21:43:53 -0500

    create a folder to keep track of different html elements so they can be used across multiple files with ease

---
+++ to-do-list/src/htmlElements/inputs.ts
@@ -0,0 +1,4 @@
+export const setEmail = document.getElementById("setEmail") as HTMLInputElement
+export const setUsername = document.getElementById("setUsername") as HTMLInputElement
+export const setPassword = document.getElementById("setPassword") as HTMLInputElement
+
