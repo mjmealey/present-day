71b6c22f9dd291bbc799d7a9bbb9b742fe74662d
mjmealey <markjmealey@gmail.com>
2023-09-12 16:23:53 -0500

    remove inputs.ts file to avoid overcomplicating eachinput field's ID

--- to-do-list/src/htmlElements/inputs.ts
+++
@@ -1,4 +0,0 @@
-export const setEmail = document.getElementById("setEmail") as HTMLInputElement
-export const setUsername = document.getElementById("setUsername") as HTMLInputElement
-export const setPassword = document.getElementById("setPassword") as HTMLInputElement
-
