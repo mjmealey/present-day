5fb410a8dfecbd9d4184b75085fa774b9969241b
mjmealey <markjmealey@gmail.com>
2023-09-11 21:42:57 -0500

    create enums folder for objects

---
+++ to-do-list/src/enums/ErrorMessages.ts
@@ -0,0 +1,5 @@
+export enum EmailErrorMessages {
+    invalidPattern = "Please enter a valid email address ex: example@gmail.com",
+    emptyField = "You must enter an email address",
+    overCharacterLimit = "Email is too long"
+}
