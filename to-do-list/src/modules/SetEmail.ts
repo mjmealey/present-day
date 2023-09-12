1bc82985ee63db08ae1a6cb0f5d039bcd828c711
mjmealey <markjmealey@gmail.com>
2023-09-12 16:22:41 -0500

    pattern validation complete for email

--- to-do-list/src/modules/SetEmail.ts
+++ to-do-list/src/modules/SetEmail.ts
@@ -1,23 +1,37 @@
-import { setEmail } from "../htmlElements/inputs.js";
 import { emailError } from "../htmlElements/errorMessage.js";
+import { formState } from "../objects/formState.js";
 
 const SetEmail = () => {
-    
+  const setEmail = document.getElementById("setEmail") as HTMLInputElement;
+  const errorMessage = formState.email.errorMessages;
 
-    const validateEmailPattern = () => {
-        const email = setEmail.value.trim()
-        const pattern = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/ 
-        const isValidEmail = pattern.test(email)
-        if(isValidEmail){
-            if(emailError){
-                
-            }
-        } else {
-
-        }
+  const validatePattern = () => {
+    const email = setEmail.value.trim();
+    const pattern: RegExp = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const isValidEmail = pattern.test(email);
+    if (isValidEmail || setEmail.value === "") {
+      if (emailError) {
+        emailError.innerText = "";
+      }
+    } else {
+      if (emailError) {
+        const patternError = errorMessage.invalidPattern;
+        emailError.innerText = patternError;
+      }
     }
+  };
 
+  const validateCharacterLimit = () => {
+    
+  }
 
-}
+  setEmail.addEventListener("input", () => {
+    validatePattern();
+  });
 
-export default SetEmail
+  setEmail.addEventListener("blur", () => {
+    validatePattern();
+  });
+};
+
+export default SetEmail;
