ee0d6f5371dacf1655cc2b6bf8d7897e25444160
mjmealey <markjmealey@gmail.com>
2023-09-16 16:57:52 -0500

    jest installed for testing during the project

---
+++ present-day/__tests__/ValidateEmail.test.ts
@@ -0,0 +1,10 @@
+
+import { describe, expect, test} from "@jest/globals"
+
+describe("ValidateEmails", () => {
+  
+  test("validatePattern", () => {
+    const sum = 1 + 2
+    expect(sum).toEqual(3)
+  })
+})
