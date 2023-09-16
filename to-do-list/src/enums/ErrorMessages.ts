bc3e07ef6dea82a5f0fbf1dee5c2751bfc2e26e8
mjmealey <markjmealey@gmail.com>
2023-09-16 18:58:30 -0500

    files deleted after project change

--- to-do-list/src/enums/ErrorMessages.ts
+++
@@ -1,5 +0,0 @@
-export enum EmailErrorMessages {
-    invalidPattern = "Please enter a valid email address ex: example@gmail.com",
-    emptyField = "You must enter an email address",
-    overCharacterLimit = "Email is too long"
-}
