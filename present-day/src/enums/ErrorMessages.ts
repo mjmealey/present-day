86eb6c43c6e6b0c8660d75984416da130ee0b033
mjmealey <markjmealey@gmail.com>
2023-09-17 16:41:10 -0500

    new error message made for the domain character limit error

--- present-day/src/enums/ErrorMessages.ts
+++ present-day/src/enums/ErrorMessages.ts
@@ -3,6 +3,7 @@ export enum EmailErrorMessages {
     emptyField = "You must enter an email address",
     overCharacterLimit = "Email is too long (at most 320 characters)",
     underCharacterLimitPreAtSymbol = "Email is too short prior to the @ symbol, (at least 5 characters) this is meant to help with security and by extension maintain a unique user email",
+    underCharacterLimitForDomain = "Email is too short after the @ symbol. Make sure the domain you have entered is recognizable. ex: gmail, yahoo, outlook",
     doesNotMatch = "These emails do not match"
     
 }
