d4b99ed8e721f5cbe9ead7a61a1302fecba42e67
mjmealey <markjmealey@gmail.com>
2023-09-16 19:10:30 -0500

    enums file made for error messages

--- present-day/src/enums/ErrorMessages.ts
+++ present-day/src/enums/ErrorMessages.ts
@@ -1,5 +1,7 @@
 export enum EmailErrorMessages {
     invalidPattern = "Please enter a valid email address ex: example@gmail.com",
     emptyField = "You must enter an email address",
-    overCharacterLimit = "Email is too long"
+    overCharacterLimit = "Email is too long (at most 320 characters)",
+    underCharacterLimit = "Email is too short (at least 5 characters)",
+    doesNotMatch = "These emails do not match"
 }
