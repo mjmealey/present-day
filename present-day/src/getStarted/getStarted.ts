42472879ad352daf0fe0ab905897c0330decd8d5
mjmealey <markjmealey@gmail.com>
2023-09-16 21:09:08 -0500

    file changed to house each individual main email function

--- present-day/src/getStarted/getStarted.ts
+++ present-day/src/getStarted/getStarted.ts
@@ -1,24 +1,7 @@
-import ValidateEmail from "./email/ValidateEmail.js";
-import confirmEmails from "./email/confirmEmails.js";
-import { signUpForm, setEmail, confirmEmail } from "./getStartedFormIds.js";
-
-const emails = (signUpFormId: HTMLElement) => {
-  const isSetEmailTarget = signUpFormId === setEmail;
-  const isConfirmEmailTarget = signUpFormId === confirmEmail;
-  if (isSetEmailTarget) {
-    ValidateEmail();
-  } else if (isConfirmEmailTarget) {
-    confirmEmails();
-  } 
-};
+import { validateEmail } from "./email/validation/validateEmail.js"
 
 const getStarted = () => {
-  signUpForm.addEventListener("input", (e) => {
-    const validHTMLElement = e.target instanceof HTMLElement;
-    if (validHTMLElement) {
-      emails(e.target);
-    }
-  });
-};
+  validateEmail()
+}
 
-getStarted();
+getStarted()
