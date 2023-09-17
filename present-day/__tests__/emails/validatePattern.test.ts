b5adeb8db1b1ee8d5a01c23139594a4d4c5dc4a3
mjmealey <markjmealey@gmail.com>
2023-09-16 21:03:52 -0500

    new folder made for testing email files

--- present-day/__tests__/emails/validatePattern.test.ts
+++ present-day/__tests__/emails/validatePattern.test.ts
@@ -3,14 +3,12 @@ describe("ValidateEmail", () => {
   test("validatePattern", () => {
     const pattern: RegExp = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
 
-
     interface ValidEmails {
       1: string;
       2: string;
       3: string;
       4: string;
     }
-
 
     const validEmails: ValidEmails = {
       1: "jakemealey@gmail.com",
@@ -22,7 +20,7 @@ describe("ValidateEmail", () => {
     const validEmailOne: string = validEmails[1];
     const validEmailTwo: string = validEmails[2];
     const validEmailThree: string = validEmails[3];
-    const validEmailFour : string= validEmails[4];
+    const validEmailFour: string = validEmails[4];
 
     const isValidPatternOne: boolean = pattern.test(validEmailOne);
     const isValidPatternTwo: boolean = pattern.test(validEmailTwo);
@@ -44,26 +42,20 @@ describe("ValidateEmail", () => {
       1: "jakemealey.com",
       2: "bobmayor@",
       3: "bobmaras@.cos",
-      4: "seniorjesse@gmail"
-    }
+      4: "seniorjesse@gmail",
+    };
 
-    const invalidEmailOne: string = invalidEmails[1]
-    const invalidEmailTwo: string = invalidEmails[2]
-    const invalidEmailThree: string = invalidEmails[3]
-    const invalidEmailFour: string = invalidEmails[4]
-    const isInvalidPatternOne: boolean = pattern.test(invalidEmailOne)
-    const isInvalidPatternTwo: boolean = pattern.test(invalidEmailTwo)
-    const isInvalidPatternThree: boolean = pattern.test(invalidEmailThree)
-    const isInvalidPatternFour: boolean = pattern.test(invalidEmailFour)
-    expect(isInvalidPatternOne).not.toBe(true)
-    expect(isInvalidPatternTwo).not.toBe(true)
-    expect(isInvalidPatternThree).not.toBe(true)
-    expect(isInvalidPatternFour).not.toBe(true)
-    
+    const invalidEmailOne: string = invalidEmails[1];
+    const invalidEmailTwo: string = invalidEmails[2];
+    const invalidEmailThree: string = invalidEmails[3];
+    const invalidEmailFour: string = invalidEmails[4];
+    const isInvalidPatternOne: boolean = pattern.test(invalidEmailOne);
+    const isInvalidPatternTwo: boolean = pattern.test(invalidEmailTwo);
+    const isInvalidPatternThree: boolean = pattern.test(invalidEmailThree);
+    const isInvalidPatternFour: boolean = pattern.test(invalidEmailFour);
+    expect(isInvalidPatternOne).not.toBe(true);
+    expect(isInvalidPatternTwo).not.toBe(true);
+    expect(isInvalidPatternThree).not.toBe(true);
+    expect(isInvalidPatternFour).not.toBe(true);
   });
-  test("validateMinCharLimit", () => {
-     const minLength: number = 5
-     const reachedMinCharLimit = "a".repeat(minLength)
-     
-  })
 });
