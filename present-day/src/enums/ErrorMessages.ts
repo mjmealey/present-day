e67495d94321be5b50674008c0916d8c8696e083
mjmealey <markjmealey@gmail.com>
2023-09-17 15:21:18 -0500

    underCharacterLimit enum variable changed to underCharacterLimitPreAtSymbol

--- present-day/src/enums/ErrorMessages.ts
+++ present-day/src/enums/ErrorMessages.ts
@@ -2,6 +2,7 @@ export enum EmailErrorMessages {
     invalidPattern = "Please enter a valid email address ex: example@gmail.com",
     emptyField = "You must enter an email address",
     overCharacterLimit = "Email is too long (at most 320 characters)",
-    underCharacterLimit = "Email is too short (at least 5 characters)",
+    underCharacterLimitPreAtSymbol = "Email is too short prior to the @ symbol, (at least 5 characters) this is meant to help with security and by extension maintain a unique user email",
     doesNotMatch = "These emails do not match"
+    
 }
