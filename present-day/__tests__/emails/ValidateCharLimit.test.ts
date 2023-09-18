6135374fff22502c906ac89a0d5004f777732754
mjmealey <markjmealey@gmail.com>
2023-09-17 21:06:52 -0500

    char limit testing logic  finished. refactoring tomorrow

--- present-day/__tests__/emails/ValidateCharLimit.test.ts
+++ present-day/__tests__/emails/ValidateCharLimit.test.ts
@@ -2,52 +2,80 @@ import { describe, test, expect } from "@jest/globals";
 
 describe("ValidateCharLimit", () => {
   test("validateLocalMinCharLimit", () => {
-    const minLocalLength = 5;
-    const validEmails = {
-      1: "markj@gmail.com",
-      2: "toddbeast@gmail.com",
-      3: "marcopollo@yahoo.com",
-      4: "jamesmark@outlook.com",
-    };
-    const isValidLengthOne = validEmails[1].split("@")[0];
-    const isValidLengthTwo = validEmails[2].split("@")[0];
-    const isValidLengthThree = validEmails[3].split("@")[0];
-    const isValidLengthFour = validEmails[4].split("@")[0];
-    expect(isValidLengthOne.length).toBeLessThanOrEqual(minLocalLength);
-    expect(isValidLengthTwo.length).toBeGreaterThanOrEqual(minLocalLength);
-    expect(isValidLengthThree.length).toBeGreaterThanOrEqual(minLocalLength);
-    expect(isValidLengthFour.length).toBeGreaterThanOrEqual(minLocalLength);
+    const minLocalLength: number = 5;
+    const validEmails: string[] = [
+      "markj@gmail.com",
+      "toddbeast@gmail.com",
+      "marcopollo@yahoo.com",
+      "jamesmark@outlook.com",
+    ];
 
-    const invalidEmails = {
-      1: "mark@gmail.com",
-      2: "bob@gmail.com",
-      3: "be@gmail.com",
-      4: "a@outlook.com",
-    };
+    validEmails.forEach((index: string) => {
+      const local: string = index.split("@")[0];
+      expect(local.length).toBeGreaterThanOrEqual(minLocalLength);
+    });
 
-    const isInvalidLengthOne = invalidEmails[1].split("@")[0];
-    const isInvalidLengthTwo = invalidEmails[2].split("@")[0];
-    const isInvalidLengthThree = invalidEmails[3].split("@")[0];
-    const isInvalidLengthFour = invalidEmails[4].split("@")[0];
-    expect(isInvalidLengthOne.length).not.toBeGreaterThanOrEqual(
-      minLocalLength
-    );
-    expect(isInvalidLengthTwo.length).not.toBeGreaterThanOrEqual(
-      minLocalLength
-    );
-    expect(isInvalidLengthThree.length).not.toBeGreaterThanOrEqual(
-      minLocalLength
-    );
-    expect(isInvalidLengthFour.length).not.toBeGreaterThanOrEqual(
-      minLocalLength
-    );
+    const invalidEmails: string[] = [
+      "mark@gmail.com",
+      "bob@gmail.com",
+      "be@gmail.com",
+      "a@outlook.com",
+    ];
+
+    invalidEmails.forEach((index: string) => {
+      const local: string = index.split("@")[0];
+      expect(local.length).toBeLessThanOrEqual(minLocalLength);
+    });
   });
 
   test("validateDomainMinCharLimit", () => {
-       const minDomainLength = 3
-       const validEmails = {
-            1: "markjmealey@gmail.com",
-            2: ""
-       } 
-  })
+    const minDomainLength: number = 3;
+    const validEmails: string[] = [
+      "markjmealey@gmail.com",
+      "markjm@outlook.com",
+      "bobtrouser@yahoo.com",
+      "tillpickles@hotmail.com",
+    ];
+
+    validEmails.forEach((index: string) => {
+      const focusOnAtSymbol: number = index.indexOf(".");
+      const isDotIncluded: boolean = focusOnAtSymbol !== -1;
+      if (isDotIncluded) {
+        const isIncluded: string = index.split("@")[1];
+        const focusOnDomain: string = isIncluded.split(".")[0];
+        expect(focusOnDomain.length).toBeGreaterThanOrEqual(minDomainLength);
+      }
+    });
+
+    const invalidEmails: string[] = [
+      "markjmealey@gma.com",
+      "markjm@ou.com",
+      "bobtrouser@yah.com",
+      "tillpickles@h.com",
+    ];
+
+    invalidEmails.forEach((index: string) => {
+      const focusOnAtSymbol: number = index.indexOf(".");
+      const isDotIncluded: boolean = focusOnAtSymbol !== -1;
+      if (isDotIncluded) {
+        const isIncluded: string = index.split("@")[1];
+        const focusOnDomain: string = isIncluded.split(".")[0];
+        expect(focusOnDomain.length).toBeLessThanOrEqual(minDomainLength);
+      }
+    });
+  });
+
+  test("validateMaxCharLimit", () => {
+    const maxLength = 320;
+    const validEmails = [
+      "bobross@outlook.com",
+      "timbowie@gmail.com",
+      "tedsour@hotmail.com",
+      "billsagot@yahoo.com",
+    ];
+
+    validEmails.forEach((index: string) => {
+      expect(index.length).toBeLessThanOrEqual(maxLength);
+    });
+  });
 });
