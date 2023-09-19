99df0c03919921e9611644810a2e4851505ea2d0
mjmealey <markjmealey@gmail.com>
2023-09-18 19:56:16 -0500

    underCharacterLimitPreAtSymbol and underCharacterLimitForDomain strings shortened

--- present-day/src/enums/ErrorMessages.ts
+++ present-day/src/enums/ErrorMessages.ts
@@ -2,8 +2,8 @@ export enum EmailErrorMessages {
     invalidPattern = "Please enter a valid email address ex: example@gmail.com",
     emptyField = "You must enter an email address",
     overCharacterLimit = "Email is too long (at most 320 characters)",
-    underCharacterLimitPreAtSymbol = "Email is too short prior to the @ symbol, (at least 5 characters) this is meant to help with security and by extension maintain a unique user email",
-    underCharacterLimitForDomain = "Email is too short after the @ symbol. Make sure the domain you have entered is recognizable. ex: gmail, yahoo, outlook",
+    underCharacterLimitPreAtSymbol = "Email is too short prior to the @, (at least 5 characters)",
+    underCharacterLimitForDomain = "Email is too short after the @. Is your domain valid? ex: gmail, outlook",
     doesNotMatch = "These emails do not match"
     
 }
