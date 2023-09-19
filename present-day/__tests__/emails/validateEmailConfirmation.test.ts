d7900bd37ec919ed5a810f1ebbf1a4c1bf05628e
mjmealey <markjmealey@gmail.com>
2023-09-18 20:08:42 -0500

    test files moved to jest folder

--- present-day/__tests__/emails/validateEmailConfirmation.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@jest/globals";
-
-test("validateEmailConfirmation", () => {
-  const firstEmail: string = "markj@gmail.com";
-  const secondEmail: string = "markj@gmail.com";
-
-  const matchingEmails: boolean = firstEmail === secondEmail;
-  expect(matchingEmails).toBe(true);
-});
