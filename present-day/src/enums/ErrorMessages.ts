75a6648448073ef7ab13f0d999be6b8767bb5a45
mjmealey <markjmealey@gmail.com>
2023-09-18 21:15:51 -0500

    code formatted

--- present-day/src/enums/ErrorMessages.ts
+++ present-day/src/enums/ErrorMessages.ts
@@ -1,9 +1,8 @@
 export enum EmailErrorMessages {
-    invalidPattern = "Please enter a valid email address ex: example@gmail.com",
-    emptyField = "You must enter an email address",
-    overCharacterLimit = "Email is too long (at most 320 characters)",
-    underCharacterLimitPreAtSymbol = "Email is too short prior to the @, (at least 5 characters)",
-    underCharacterLimitForDomain = "Email is too short after the @. Is your domain valid? ex: gmail, outlook",
-    doesNotMatch = "These emails do not match"
-    
+  invalidPattern = "Please enter a valid email address ex: example@gmail.com",
+  emptyField = "You must enter an email address",
+  overCharacterLimit = "Email is too long (at most 320 characters)",
+  underCharacterLimitPreAtSymbol = "Email is too short prior to the @, (at least 5 characters)",
+  underCharacterLimitForDomain = "Email is too short after the @. Is your domain valid? ex: gmail, outlook",
+  doesNotMatch = "These emails do not match",
 }
