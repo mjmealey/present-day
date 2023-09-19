261bb66c14981f2272a39a8416339955e597a348
mjmealey <markjmealey@gmail.com>
2023-09-18 19:53:42 -0500

    both file name and function name changed due to validatePattern not being a module

--- present-day/src/getStarted/email/validation/validatePattern.ts
+++ present-day/src/getStarted/email/validation/validatePattern.ts
@@ -4,10 +4,9 @@ import { removeErrors } from "../../../formElements/manageErrors.js";
 import { formState } from "../../../objects/formState.js";
 import { setEmail } from "../../getStartedFormIds.js";
 
-export const ValidatePattern = () => {
+export const validatePattern = () => {
   const email: string = setEmail.value.trim();
-  //can contain special characters such as - and +
-  const pattern: RegExp = /^[\w\.-+]+@[a-zA-Z\d\.-]+(\.[a-zA-Z]{2,})+$/i;
+  const pattern: RegExp =  /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
   //tests if the value of the setEmail input is valid as indicated by the pattern's content
   const isValidEmail: boolean = pattern.test(email);
   if (!isValidEmail) {
