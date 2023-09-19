941f619ca088ec7a28afdc9eb2be456b82f8221e
mjmealey <markjmealey@gmail.com>
2023-09-18 21:16:25 -0500

    testing for user email character limit complete

--- present-day/__tests__/playwright/emails/ValidateCharLimit.spec.ts
+++ present-day/__tests__/playwright/emails/ValidateCharLimit.spec.ts
@@ -1,28 +1,41 @@
 import { test, expect } from "@playwright/test";
 
-test("email error text displays that the local part of the email is under the minimum length", async ({
+test("email error text displays that the local part of the email is under the min length", async ({
   page,
 }) => {
   await page.goto("http://localhost:5173/src/getStarted/getStarted.html");
 
+  const emailPlaceholder: string = "example@gmail.com";
   const underLocalMinLength: string = "ma@";
-  await page.getByLabel("Email").fill(underLocalMinLength);
-
-  const emailError: string = "#emailError";
-
-  expect(page.locator(emailError).innerText());
+  await page.getByPlaceholder(emailPlaceholder).fill(underLocalMinLength);
+  const expectedText: string =
+    "Email is too short prior to the @, (at least 5 characters)";
+  expect(page.getByText(expectedText)).toBeVisible();
 });
 
-test("email error text displays that the domain part of the amil is under the minimum length", async ({
+test("email error text displays that the domain part of the email is under the min length", async ({
   page,
 }) => {
   await page.goto("http://localhost:5173/src/getStarted/getStarted.html");
 
-  const setEmail: string = "#setEmail";
+  const emailPlaceholder: string = "example@gmail.com";
   const underLocalDomainLength: string = "mark@gm.com";
-  await page.locator(setEmail).fill(underLocalDomainLength);
+  await page.getByPlaceholder(emailPlaceholder).fill(underLocalDomainLength);
+  const expectedText: string =
+    "Email is too short after the @. Is your domain valid? ex: gmail, outlook";
+  expect(page.getByText(expectedText)).toBeVisible();
+});
 
-  const emailError: string = "#emailError";
+test("email error text displays that the entirety of the email is over the max length", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173/src/getStarted/getStarted.html");
 
-  expect(page.locator(emailError).innerText());
+  const emailPlaceholder: string = "example@gmail.com";
+  const overMainDomainLength: string = "a".repeat(320);
+  await page.getByPlaceholder(emailPlaceholder).fill(overMainDomainLength);
+
+  const expectedText = "Email is too long (at most 320 characters)";
+
+  expect(page.getByText(expectedText)).toBeVisible();
 });
