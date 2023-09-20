5d45012b8181aef9b9d83551d984902a4b24a17b
mjmealey <markjmealey@gmail.com>
2023-09-19 19:40:55 -0500

    file made to store patterns for playwright testing

---
+++ present-day/__tests__/playwright/variables/patterns.ts
@@ -0,0 +1,1 @@
+export const email: RegExp =  /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
---
+++ present-day/__tests__/playwright/variables/placeholders.ts
@@ -0,0 +1,2 @@
+export const setEmailPlaceholder: string = "example@gmail.com"
+export const confirmEmailPlaceholder: string = "matching the first email"
