257d9ba0e619bb7c5f40c0cc4e0ba56758e0c06f
mjmealey <markjmealey@gmail.com>
2023-09-19 20:29:53 -0500

    new folder called colors made to give email inputs a specific color based on user input such as light green for a valid input and light red for an invalid input

---
+++ present-day/src/getStarted/email/colors/SetEmailColors.ts
@@ -0,0 +1,9 @@
+import { setEmail } from "../../getStartedFormIds.js";
+import { emailError } from "../../getStartedFormIds.js";
+
+const SetEmailColors = () => {
+
+    const validColors = () => {
+
+    }
+}
