cc7769f4c6f9c0635494891ddadeee24836879b4
mjmealey <markjmealey@gmail.com>
2023-09-19 21:20:45 -0500

    valid color function started for color validation for setEmail input

--- present-day/src/getStarted/email/colors/SetEmailColors.ts
+++ present-day/src/getStarted/email/colors/SetEmailColors.ts
@@ -1,9 +1,28 @@
 import { setEmail } from "../../getStartedFormIds.js";
 import { emailError } from "../../getStartedFormIds.js";
+import { formState } from "../../../objects/formState.js";
+import { colors } from "../../../objects/formState.js";
 
 const SetEmailColors = () => {
-
-    const validColors = () => {
-
+  const validColors = () => {
+    const isNotEmpty: boolean = setEmail.value !== formState.emptyField;
+    const isErrorFieldEmpty = emailError.innerText === formState.noErrors;
+    const isValid: boolean = isNotEmpty && isErrorFieldEmpty;
+    if (isValid) {
+      const validColor: string = colors.valid;
+      setEmail.style.backgroundColor = validColor;
+    } else {
+        const defaultColor: string = "white"
+        setEmail.style.backgroundColor = defaultColor
     }
-}
+    return validColors;
+  };
+
+  const invalidColors = () => {}
+
+  return {
+    validColors,
+  };
+};
+
+export default SetEmailColors;
