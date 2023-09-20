d0d86eed1cc3cc9786f1ee8078f61b9dced82a7d
mjmealey <markjmealey@gmail.com>
2023-09-19 21:19:16 -0500

    new functions invoked

--- present-day/src/getStarted/getStarted.ts
+++ present-day/src/getStarted/getStarted.ts
@@ -2,10 +2,12 @@ import { validateEmail } from "./email/validation/validateEmail.js";
 import { setEmail } from "./getStartedFormIds.js";
 import { validateConfirmEmail } from "./email/validation/validateEmail.js";
 import { confirmEmail } from "./getStartedFormIds.js";
+import { emailColors } from "./email/colors/emailColors.js";
 
 const getStarted = () => {
   setEmail.addEventListener("input", () => {
     validateEmail();
+    emailColors()
   });
 
   confirmEmail.addEventListener("input", () => {
