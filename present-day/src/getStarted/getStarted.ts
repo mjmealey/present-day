2cd07bd119ab6a6c49d8d1c45a7e79a489c8b90c
mjmealey <markjmealey@gmail.com>
2023-09-20 21:05:03 -0500

    new function invoked with parameters that changes the colors of the inputs based on user input

--- present-day/src/getStarted/getStarted.ts
+++ present-day/src/getStarted/getStarted.ts
@@ -1,18 +1,18 @@
-import { validateEmail } from "./email/validation/validateEmail.js";
+import { validateEmail } from "./emails/validation/validateEmail.js";
 import { setEmail } from "./getStartedFormIds.js";
-import { validateConfirmEmail } from "./email/validation/validateEmail.js";
+import { validateConfirmEmail } from "./emails/validation/validateEmail.js";
 import { confirmEmail } from "./getStartedFormIds.js";
 import { signUpInputColors } from "./signUpInputColors.js";
 import { emailError } from "./getStartedFormIds.js";
 import { matchingEmails } from "./getStartedFormIds.js";
 
 const getStarted = () => {
-  setEmail.addEventListener("blur", () => {
+  setEmail.addEventListener("input", () => {
     validateEmail();
     signUpInputColors(setEmail, emailError)
   });
 
-  confirmEmail.addEventListener("blur", () => {
+  confirmEmail.addEventListener("input", () => {
     validateConfirmEmail()
     signUpInputColors(confirmEmail, matchingEmails)
   })
