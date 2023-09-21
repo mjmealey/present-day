33d09c2ece6aff7bd5673ebb3ccf6b98cc18fac5
mjmealey <markjmealey@gmail.com>
2023-09-21 18:13:31 -0500

    event listeners changed to blur

--- present-day/src/getStarted/getStarted.ts
+++ present-day/src/getStarted/getStarted.ts
@@ -2,20 +2,20 @@ import { validateEmail } from "./emails/validation/validateEmail.js";
 import { setEmail } from "./getStartedFormIds.js";
 import { validateConfirmEmail } from "./emails/validation/validateEmail.js";
 import { confirmEmail } from "./getStartedFormIds.js";
-import { signUpInputColors } from "./signUpInputColors.js";
+import { emailInputColors } from "./emails/emailInputColors.js";
 import { emailError } from "./getStartedFormIds.js";
 import { matchingEmails } from "./getStartedFormIds.js";
 
 const getStarted = () => {
-  setEmail.addEventListener("input", () => {
+  setEmail.addEventListener("blur", () => {
     validateEmail();
-    signUpInputColors(setEmail, emailError)
+    emailInputColors(setEmail, emailError);
   });
 
-  confirmEmail.addEventListener("input", () => {
-    validateConfirmEmail()
-    signUpInputColors(confirmEmail, matchingEmails)
-  })
+  confirmEmail.addEventListener("blur", () => {
+    validateConfirmEmail();
+    emailInputColors(confirmEmail, matchingEmails);
+  });
 };
 
 getStarted();
