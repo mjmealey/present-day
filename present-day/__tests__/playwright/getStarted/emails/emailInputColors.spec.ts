58fc9a9af5c1993fdff4d21bb883008235d3bf64
mjmealey <markjmealey@gmail.com>
2023-09-21 18:28:35 -0500

    confirmEmail test complete, moving onto valid inputs for green border color

--- present-day/__tests__/playwright/getStarted/emails/emailInputColors.spec.ts
+++ present-day/__tests__/playwright/getStarted/emails/emailInputColors.spec.ts
@@ -4,7 +4,7 @@ import {
   confirmEmailPlaceholder,
 } from "../../variables/placeholders.js";
 import { signUpPage } from "../../variables/urls.js";
-import { setEmail } from "../../variables/inputIds.js";
+import { confirmEmail, setEmail } from "../../variables/inputIds.js";
 import { colors } from "../../../../src/objects/formState.js";
 
 test.beforeEach(async ({ page }) => {
@@ -20,8 +20,8 @@ test("add a crimson border color if the email error text is visible for errors",
   const invalidPattern: string = "ma";
 
   await page.getByPlaceholder(setEmailPlaceholder).fill(invalidPattern);
+  await page.getByPlaceholder(setEmailPlaceholder).blur();
   const setEmailInput: Locator = page.locator(setEmail);
-  await page.getByPlaceholder(setEmailPlaceholder).blur()
   expect(await setEmailInput.evaluate((email) => email.style.borderColor)).toBe(
     invalidColor
   );
@@ -34,8 +34,8 @@ test("add a crimson border if the email is under the min length pre @ symbol", a
   await page
     .getByPlaceholder(setEmailPlaceholder)
     .fill(underMinLengthPreAtSymbol);
+  await page.getByPlaceholder(setEmailPlaceholder).blur();
   const setEmailInput: Locator = page.locator(setEmail);
-  await page.getByPlaceholder(setEmailPlaceholder).blur()
   expect(await setEmailInput.evaluate((email) => email.style.borderColor)).toBe(
     invalidColor
   );
@@ -45,9 +45,9 @@ test("add a crimson border if the domain is under the min length", async ({
   page,
 }) => {
   const underMinDomainLength: string = "markjmealey@gm.com";
-  const setEmailInput: Locator = page.locator(setEmail);
   await page.getByPlaceholder(setEmailPlaceholder).fill(underMinDomainLength);
-  await page.getByPlaceholder(setEmailPlaceholder).blur()
+  await page.getByPlaceholder(setEmailPlaceholder).blur();
+  const setEmailInput: Locator = page.locator(setEmail);
   expect(await setEmailInput.evaluate((email) => email.style.borderColor)).toBe(
     invalidColor
   );
@@ -58,14 +58,25 @@ test("add a crimson border if the email is over or equal to the max length", asy
 }) => {
   const maxLength: string = "a".repeat(320);
   await page.getByPlaceholder(setEmailPlaceholder).fill(maxLength);
+  await page.getByPlaceholder(setEmailPlaceholder).blur();
   const setEmailInput: Locator = page.locator(setEmail);
-  await page.getByPlaceholder(setEmailPlaceholder).blur()
   expect(await setEmailInput.evaluate((email) => email.style.borderColor)).toBe(
     invalidColor
   );
 });
 
 //confirmEmail
-test("add a crimson border if the confirmEmail input does not match the setEmail input", async ({page}) => {
-
-})  
+test("add a crimson border if the confirmEmail input does not match the setEmail input", async ({
+  page,
+}) => {
+  const firstEmail: string = "markjm@gmail.com";
+  const secondEmail: string = "markjn@gmail.com"
+  
+  await page.getByPlaceholder(setEmailPlaceholder).fill(firstEmail);
+  await page.getByPlaceholder(confirmEmailPlaceholder).fill(secondEmail);
+  await page.getByPlaceholder(confirmEmailPlaceholder).blur();
+  const confirmEmailInput: Locator = page.locator(confirmEmail);
+  expect(
+    await confirmEmailInput.evaluate((email) => email.style.borderColor)
+  ).toBe(invalidColor);
+});
