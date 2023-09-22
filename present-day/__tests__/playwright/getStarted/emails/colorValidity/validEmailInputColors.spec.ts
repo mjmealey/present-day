1601038cba4850090440c8dade1c9f26d66d56f6
mjmealey <markjmealey@gmail.com>
2023-09-21 19:52:23 -0500

    valid setEmail input testing complete for light green border color

--- present-day/__tests__/playwright/getStarted/emails/colorValidity/validEmailInputColors.spec.ts
+++ present-day/__tests__/playwright/getStarted/emails/colorValidity/validEmailInputColors.spec.ts
@@ -6,3 +6,20 @@ import {
 import { signUpPage } from "../../../variables/urls.js";
 import { confirmEmail, setEmail } from "../../../variables/inputIds.js";
 import { colors } from "../../../../../src/objects/formState.js";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(signUpPage);
+});
+
+const validColor: string = colors.valid;
+test("add a light green border color to the setEmail input if there is a valid input", async ({
+  page,
+}) => {
+  const validEmail: string = "bobjames@gmail.com";
+  await page.getByPlaceholder(setEmailPlaceholder).fill(validEmail);
+  await page.getByPlaceholder(setEmailPlaceholder).blur()
+  const setEmailInput: Locator = page.locator(setEmail)
+  expect(await setEmailInput.evaluate((email) => email.style.borderColor)).toBe(validColor)  
+});
+
+test("add a light green border to the confirmEmail input if there is a correct input based on a matching email", () => {})
