91ceb132b61535c2d145a1945ef90e35b0e29907
mjmealey <markjmealey@gmail.com>
2023-09-23 00:22:47 -0500

    properties added to assure module is possible for jest

--- present-day/jest.config.ts
+++ present-day/jest.config.ts
@@ -1,9 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': "ts-jest",
+    "^.+\\.jsx?$": "babel-jest",
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+      esModuleInterop: true,
+      allowSyntheticDefaultImports: true
+    }
+  }
 }
