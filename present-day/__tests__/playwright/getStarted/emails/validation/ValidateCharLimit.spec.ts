f0d8cd07e62bc0558111f19b9038bd9bbe0ffbf7
mjmealey <markjmealey@gmail.com>
2023-09-23 00:24:35 -0500

    testing done to assure jest is working as intended

--- present-day/__tests__/playwright/getStarted/emails/validation/ValidateCharLimit.spec.ts
+++ present-day/__tests__/playwright/getStarted/emails/validation/ValidateCharLimit.spec.ts
@@ -10,6 +10,7 @@ test("email error text displays that the local part of the email is under the mi
 }) => {
   const underLocalMinLength: string = "ma@";
   await page.getByPlaceholder(setEmailPlaceholder).fill(underLocalMinLength);
+  await page.getByPlaceholder(setEmailPlaceholder).blur()
   const expectedText: string =
     "Email is too short prior to the @, (at least 5 characters)";
   expect(page.getByText(expectedText)).toBeVisible();
@@ -20,6 +21,7 @@ test("email error text displays that the domain part of the email is under the m
 }) => {
   const underLocalDomainLength: string = "mark@gm.com";
   await page.getByPlaceholder(setEmailPlaceholder).fill(underLocalDomainLength);
+  await page.getByPlaceholder(setEmailPlaceholder).blur()
   const expectedText: string =
     "Email is too short after the @. Is your domain valid? ex: gmail, outlook";
   expect(page.getByText(expectedText)).toBeVisible();
@@ -31,7 +33,7 @@ test("email error text displays that the entirety of the email is over the max l
   const maxLength: number = 320;
   const overMainDomainLength: string = "a".repeat(maxLength);
   await page.getByPlaceholder(setEmailPlaceholder).fill(overMainDomainLength);
-
+  await page.getByPlaceholder(setEmailPlaceholder).blur()
   const expectedText = "Email is too long (at most 320 characters)";
 
   expect(page.getByText(expectedText)).toBeVisible();
